1b0c520dff1ef3a0db70d69780ebda24bca9d506
Kalyani Kajawe <kajawek@gmail.com>
2021-01-23 04:53:34 +0100

    user_story [issue #13] : Resolving Small Issues #13
    Functionality: Resolved Small issues
    
    Tech Description:
    - added dependecy for log to optimize code
    - hide keypad On login button press
    - replaced log with Timber and handle Error handling

--- app/src/main/java/com/example/saitowapp/repository/OrderDataSource.kt
+++ app/src/main/java/com/example/saitowapp/repository/OrderDataSource.kt
@@ -1,20 +1,23 @@
 package com.example.saitowapp.repository
 
-import android.util.Log
 import androidx.paging.ItemKeyedDataSource
 import com.example.saitowapp.model.DataOrders
 import com.example.saitowapp.model.Orders
 import com.example.saitowapp.network.ApiService
 import com.example.saitowapp.network.RetrofitBuilder
-
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import retrofit2.Response
+import timber.log.Timber
 import kotlin.coroutines.CoroutineContext
 
 
-class OrderDataSource(coroutineContext: CoroutineContext, val sort:String) : ItemKeyedDataSource<Int, DataOrders>() {
+class OrderDataSource(
+    coroutineContext: CoroutineContext,
+    val sort: String,
+    val token: String
+) : ItemKeyedDataSource<Int, DataOrders>() {
     private val job = Job()
     private val scope = CoroutineScope(coroutineContext + job)
     private val apiService = RetrofitBuilder.getRetrofit().create(ApiService::class.java)
@@ -22,25 +25,28 @@ class OrderDataSource(coroutineContext: CoroutineContext, val sort:String) : Ite
         return item.id
     }
 
-    override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<DataOrders>) {
+    override fun loadInitial(
+        params: LoadInitialParams<Int>,
+        callback: LoadInitialCallback<DataOrders>
+    ) {
         scope.launch {
             try {
-                val response: Response<Orders> = apiService.getOrders(0, params.requestedLoadSize,sort)
+                val response: Response<Orders> =
+                    apiService.getOrders(token,0, params.requestedLoadSize, sort)
                 when {
                     response.isSuccessful -> {
                         val ordersList = response.body() as Orders
                         callback.onResult(ordersList.data)
                     }
                     else -> {
-                        Log.e("UserDataSource", "Error in initial load!")
-                        Log.e("UserDataSource", response.message())
-
+                        Timber.tag("ResponseUnsuccess")
+                            .e(response.message() ?: "Error in initial load!ing!")
                     }
-
                 }
 
             } catch (exception: Exception) {
-                Log.e("UserDataSource", "Exception : Failed to fetch data!")
+                Timber.tag("ExceptionOrderList")
+                    .e(exception.message.toString() ?: "Exception : Failed to fetch data!")
             }
 
         }
@@ -49,20 +55,22 @@ class OrderDataSource(coroutineContext: CoroutineContext, val sort:String) : Ite
     override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<DataOrders>) {
         scope.launch {
             try {
-                val response: Response<Orders> = apiService.getOrders(params.key - 1, params.requestedLoadSize,sort)
+                val response: Response<Orders> =
+                    apiService.getOrders(token,params.key - 1, params.requestedLoadSize, sort)
                 when {
                     response.isSuccessful -> {
                         val ordersList = response.body() as Orders
                         callback.onResult(ordersList.data)
                     }
                     else -> {
-                        Log.e("UserDataSource", "Error in After load!")
+                        Timber.tag("ResponseUnsuccess")
+                            .e(response.message() ?: "Error in After loading!!")
                     }
                 }
 
             } catch (exception: Exception) {
-                Log.e("UserDataSource", "Exception : Failed to fetch data!")
-                Log.e("UserDataSource", exception.message.toString())
+                Timber.tag("ExceptionOrderList")
+                    .e(exception.message.toString() ?: "Exception : Failed to fetch data!")
             }
 
         }
